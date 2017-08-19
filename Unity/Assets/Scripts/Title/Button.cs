b7fc0a5d4ca461a74bf2b5a675696c7f4b8bfd5c
yamagishitatsuya <az7toco90zn330dtc@gmail.com>
2017-08-20 07:41:38 +0900

    Startボタンの作成

---
+++ Unity/Assets/Scripts/Title/Button.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Button : MonoBehaviour {
+
+	public void ButtonPush()
+	{
+		Debug.Log ("test");
+		SceneManager.LoadScene("GameMain");
+	}
+}
