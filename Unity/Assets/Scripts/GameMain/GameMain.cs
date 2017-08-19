b6e520c77cf93d260e1b06599257c0780d940bf3
hykw <a55578541@gmail.com>
2017-08-19 19:31:11 +0900

    player

--- Unity/Assets/Scripts/GameMain/GameMain.cs
+++ Unity/Assets/Scripts/GameMain/GameMain.cs
@@ -9,13 +9,26 @@ using Ionic.Zlib;
 
 public class GameMain : MonoBehaviour
 {
+    public GameObject m_player;
+    public Camera m_camera;
+    Vector3 m_posi;
+
 	void Start ()
 	{
 		if (!GlobalObject.InitGlobalObject()) return;
+        m_player = GameObject.Find("Player");
+        m_posi = m_player.transform.position;
+        m_camera = Camera.main;
     }
 	
 	// Update is called once per frame
 	void Update ()
 	{
+        m_player.transform.position = new Vector3(m_posi.x, m_posi.y, m_posi.z);
+        m_posi.x += Time.deltaTime;
+        //if(Screen.width / 2 < m_player.transform)
+        //{
+        //    m_camera.transform.position = new Vector3(m_player.transform.position.x, m_camera.transform.position.y, m_camera.transform.position.z);
+        //}
 	}
 }
