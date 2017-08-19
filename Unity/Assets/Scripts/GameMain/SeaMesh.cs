c14efe902e63a63a9741e2c3ebd663fde90feae8
TakaakiHoshiyama <hoshiyama@pickle.ne.jp>
2017-08-19 20:31:43 +0900

    波の高さの取得とタッチ制御（サンプル）

--- Unity/Assets/Scripts/GameMain/SeaMesh.cs
+++ Unity/Assets/Scripts/GameMain/SeaMesh.cs
@@ -8,11 +8,37 @@ public class SeaMesh : MonoBehaviour
 	private	Mesh			m_Mesh;
 	private	float			m_Scale;
 	private	int				m_Width, m_Height;
-	private	float			m_Value, m_Counter;
+	//private	float			m_Value, m_Counter;
 	private	float[]			m_HeightValue;
 	private	float[]			m_HeightForce;
 
-	private void Start ()
+	// 波の高さの取得(World座標)
+	public float GetWaveHeight(Vector3 pos)
+	{
+		pos -= transform.localPosition;
+		pos /= m_Scale;
+		var idx = (int)pos.x;
+
+		return (0 <= idx && idx < m_HeightForce.Length)? m_HeightForce[idx] : 0;
+	}
+
+	public void ForcePower(Vector3 pos, float power)
+	{
+		pos -= transform.localPosition;
+		pos /= m_Scale;
+		var idx = (int)pos.x;
+		if (idx != 0)
+		{
+			m_HeightForce[idx - 1] = -power * 0.5f;
+		}
+		m_HeightForce[idx] = -power;
+		if (idx < m_HeightForce.Length - 1)
+		{
+			m_HeightForce[idx + 1] = -power * 0.5f;
+		}
+	}
+
+	private void Start()
 	{
 		m_MeshFilter = GetComponent<MeshFilter>();
 
@@ -42,8 +68,8 @@ public class SeaMesh : MonoBehaviour
 		{
 			uv[i + 0] = new Vector2(0, 0);
 			uv[i + 1] = new Vector2(1, 0);
-			uv[i + 2] = new Vector2(0, 1);
-			uv[i + 3] = new Vector2(1, 1);
+			uv[i + 2] = new Vector2(1, 1);
+			uv[i + 3] = new Vector2(0, 1);
 		}
 		m_Mesh.uv = uv;
 
@@ -51,12 +77,12 @@ public class SeaMesh : MonoBehaviour
 		var tri = new int[numOfTrianfles];
 		for (int i = 0, idx = 0; i < numOfTrianfles; i += 6, idx += 4)
 		{
-			tri[i + 0] = idx + 0;
-			tri[i + 1] = idx + 1;
-			tri[i + 2] = idx + 2;
-			tri[i + 3] = idx + 0;
-			tri[i + 4] = idx + 2;
-			tri[i + 5] = idx + 3;
+			tri[i + 5] = idx + 0;
+			tri[i + 4] = idx + 1;
+			tri[i + 3] = idx + 2;
+			tri[i + 2] = idx + 0;
+			tri[i + 1] = idx + 2;
+			tri[i + 0] = idx + 3;
 		}
 		m_Mesh.triangles = tri;
  
@@ -64,16 +90,20 @@ public class SeaMesh : MonoBehaviour
 		m_Mesh.RecalculateBounds();
  
 		m_MeshFilter.sharedMesh = m_Mesh;
+
+		//var meshRender = GetComponent<MeshRenderer>();
+		//meshRender.material.SetTexture("sea", Resources.Load<Texture2D>("seaTex"));
 	}
 	
 	private void Update()
 	{
-		if (Input.GetButtonDown("Jump"))
+		if (Input.GetMouseButtonDown(0))
 		{
-			m_HeightForce[5] = -1.0f;
-			m_HeightForce[6] = -2.0f;
-			m_HeightForce[7] = -1.0f;
-			Debug.Log("Pushed");
+			var	touchPos = Input.mousePosition;
+			var camera = GameObject.Find("Main Camera").GetComponent<Camera>();
+			var touchWPos = camera.ScreenToWorldPoint(new Vector3(touchPos.x, touchPos.y));
+			ForcePower(touchWPos, 2.0f);
+			//Debug.Log("Pushed" + ((int)touchWPos.x).ToString() + "," + ((int)touchWPos.y).ToString());
 		}
 		for (var i = 0; i <= m_Width; i++)
 		{
@@ -82,7 +112,7 @@ public class SeaMesh : MonoBehaviour
 			else if (m_HeightValue[i] > 0)	force = -0.10f;
 			m_HeightForce[i] += force;
 			m_HeightForce[i] *= 0.6f;
-			if (Mathf.Abs(m_HeightForce[i]) < 0.0001f)
+			if (Mathf.Abs(m_HeightForce[i]) < 0.02f)
 			{
 				m_HeightForce[i] = 0;
 				m_HeightValue[i] = 0;
