24006574b4196c3494f9f1adb8ec4dd692a5d38b
hykw <a55578541@gmail.com>
2017-08-19 20:33:20 +0900

    座標を整えた

--- Unity/Assets/Scripts/GameMain/GameMain.cs
+++ Unity/Assets/Scripts/GameMain/GameMain.cs
@@ -11,24 +11,40 @@ public class GameMain : MonoBehaviour
 {
     public GameObject m_player;
     public Camera m_camera;
-    Vector3 m_posi;
+    public GameObject m_sea;
+    Vector3 m_playerposi;
+    Vector3 m_cameraposi;
 
 	void Start ()
 	{
 		if (!GlobalObject.InitGlobalObject()) return;
         m_player = GameObject.Find("Player");
-        m_posi = m_player.transform.position;
+        m_playerposi = m_player.transform.position;
+        m_sea = GameObject.Find("sea");
         m_camera = Camera.main;
+        m_cameraposi = m_camera.transform.position;
     }
 	
 	// Update is called once per frame
 	void Update ()
 	{
-        m_player.transform.position = new Vector3(m_posi.x, m_posi.y, m_posi.z);
-        m_posi.x += Time.deltaTime;
-        //if(Screen.width / 2 < m_player.transform)
+        //if(m_camera.transform.position.x < 6)
         //{
-        //    m_camera.transform.position = new Vector3(m_player.transform.position.x, m_camera.transform.position.y, m_camera.transform.position.z);
+        //    m_camera.transform.position = new Vector3(6, m_cameraposi.y, m_cameraposi.z);
         //}
-	}
+        if(m_player.transform.position.x > 5)
+        {
+            m_camera.transform.position = new Vector3(m_player.transform.position.x, m_cameraposi.y, m_cameraposi.z);
+        }
+        m_player.transform.position = new Vector3(m_playerposi.x, m_playerposi.y, m_playerposi.z);
+        m_playerposi.x += Time.deltaTime;
+    }
+
+    void Hit()
+    {
+        if(m_player.GetComponent<Collider2D>().name == "sea")
+        {
+           
+        }
+    }
 }
