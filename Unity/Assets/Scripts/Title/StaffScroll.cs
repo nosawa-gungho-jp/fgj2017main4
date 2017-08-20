d07e593bc26b7156e659689bb2ff4e0794b1b6f0
nosawa-gungho-jp <nosawa@gungho.jp>
2017-08-20 13:59:45 +0900

    [Title]スタッフロール追加

---
+++ Unity/Assets/Scripts/Title/StaffScroll.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StaffScroll : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        var pos = transform.position;
+        pos.x -= 3 * Time.deltaTime;
+        if (pos.x < -30f)
+        {
+            pos.x += 60f;
+        }
+        transform.position = pos;
+
+    }
+}
